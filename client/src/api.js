2b7276e5df1b9b0beca564c1f6ffc09efd52018e
idaten717-cloud <idaten717@gmail.com>
2026-01-18 19:17:02 +0530

    Hardcode API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://habit-tracker-835v.onrender.com",
 });
 
 // Helper to attach token
